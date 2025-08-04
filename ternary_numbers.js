6aa1c9e268ae3cf44daee2b14126439232cd92bc
Avishek02 <avitong9@gmail.com>
2025-08-04 15:20:42 +0600

    solution of number05 added

---
+++ ternary_numbers.js
@@ -0,0 +1,8 @@
+// Problem 05 : ternary numbers 
+const num1 = 40;
+const num2 = 53;
+let result;
+
+result = num1 > num2 ? num1 * 2 : num1 + num2;
+
+console.log(`Number01 is : ${num1}\nNumber02 is : ${num2}\nResult is : ${result}`);

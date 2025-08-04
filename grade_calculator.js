1248a2fea03ce211e2168d5bfbfc3b2d5b8a456a
Avishek02 <avitong9@gmail.com>
2025-08-04 14:58:37 +0600

    solution of problem 03 added

---
+++ grade_calculator.js
@@ -0,0 +1,17 @@
+// Problem 03 : grade calculator
+const score = 59;
+let grade = "";
+
+if(score >= 90){
+    grade = "A";
+}else if(score >= 80){
+    grade = "B";
+}else if(score >= 70){
+    grade = "C";
+}else if(score >= 60){
+    grade = "D"
+}else{
+    grade = "F";
+}
+
+console.log(`Your score is : ${score}\nYour grade is : ${grade}`);
